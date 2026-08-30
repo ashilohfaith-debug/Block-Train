8856358c9085dac58dae000fe4f80439c0506dae
Kevin Joshua <kingkevinjosh7@gmail.com>
2026-08-30 21:40:41 +0530

    chore: remove junk scripts and nested submodule duplicates

--- apps/api/test.js
+++
@@ -1,1 +0,0 @@
-﻿require('dotenv').config(); const pool = require('./src/db'); pool.query(SELECT column_name, data_type FROM information_schema.columns WHERE table_name = 'active_blocks').then(res => { console.log(res.rows); process.exit(0); }).catch(console.error);
